91704f604f00cdfe7f2259ed9ab1bae5f176a18f
Santiago Manuel Castro Dau <Santiago.Castro.Dau@ibm.com>
2023-01-23 18:46:17 -0600

    Fixed download scritps

--- src/download_proteomic.sh
+++ src/download_proteomic.sh
@@ -11,5 +11,5 @@ wget https://ftp.pride.ebi.ac.uk/pride/data/archive/2022/02/PXD024043/DIANN1.8_S
 # Unzip an deleate useless data
 tar -xf DIANN1.8_SingleCells_CellCycle.7z
 mv DIANN1.8_SingleCells_CellCycle/* ./
-rm DIANN1.8_SingleCells_CellCycle*
+rm -r DIANN1.8_SingleCells_CellCycle*
 ls | grep -vE 'cellcyclepred|notnormalized' | xargs rm
--- src/download_raw.sh
+++ src/download_raw.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+ROOT=$(pwd)
 source ./src/download_proteomic.sh
+cd $ROOT
 source ./src/download_transcriptomic.sh
